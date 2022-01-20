5cb6a1e885d50f44211965e50c4e51d098ba477d
sieva-cmd <musyokasieva@gmail.com>
2022-01-20 15:28:29 +0300

    modified contact us

--- contact.js
+++ contact.js
@@ -1,11 +1,19 @@
+<<<<<<< HEAD
 $(document).ready(function(){
     function verify(){
         let name = document.getElementById('idname').value;
         let email = document.getElementById('idemail').value;
+=======
+$(document).ready(function (){
+
+        let name = document.getElementById('#idname').value;
+        let email = document.getElementById('#idemail').value;
+>>>>>>> 578bda83395cbb3591f51234a97f8e613b774789
         if(name==''){
             alert('input valid name');
             return false
         }
+<<<<<<< HEAD
         if(email==''){
             alert('input valid email');
             return false
@@ -13,3 +21,15 @@ $(document).ready(function(){
     alert('welcome '+name)
     alert('your message has been received.Thanks')
     });
+=======
+         else if(email==''){
+            alert('input valid email');
+            return false
+        }
+     else {alert('welcome '+name)
+           alert('your message has been received.Thanks')}
+    function one(){
+    
+    })
+    
+>>>>>>> 578bda83395cbb3591f51234a97f8e613b774789
