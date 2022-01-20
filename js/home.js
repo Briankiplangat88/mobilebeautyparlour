2363e7092ace0cd8551fc49c82530c907c521ece
sieva-cmd <musyokasieva@gmail.com>
2022-01-20 10:36:23 +0300

    updating chages in html

--- js/home.js
+++ js/home.js
@@ -13,12 +13,10 @@ function Booking(name, service, time, dressor, massage, others, street, city, co
 }
 
 
-
-
-
-
 //user interface
 $(document).ready(function () {
+
+
     $("#form").submit(function (event) {
         event.preventDefault();
 
@@ -31,6 +29,8 @@ $(document).ready(function () {
         var street = $("#street").val();
         var city = $("#city").val();
         var county = $("#county").val();
+
+        
 
         var fullOrder = new Booking(userName, service, timeForService, hairDressor, massager, others, street, city, county);
 
@@ -101,109 +101,3 @@ $(document).ready(function () {
 })
 
 
-
-
-// // business logic
-// function Booking(name, service, time, dressor, massage, others, street, city, county, email) {
-//     this.name = name;
-//     this.service = service;
-//     this.time = time;
-//     this.dressor = dressor;
-//     this.massage = massage;
-//     this.others = others;
-//     this.street = street;
-//     this.city = city;
-//     this.county = county;
-//     this.email = email;
-// }
-
-// var yourOder;
-
-
-// // user interface
-// $(document).ready(function () {
-
-
-//     $("#form").submit(function (event) {
-//         event.preventDefault();
-
-//         var fullname = $("#name").val();
-//         var service = $("#service").val();
-//         var timeForService = $("#timeForServie").val();
-//         var hairDressor = $("#hair-dressor").val();
-//         var masseuse = $("#masseuse").val();
-//         var otherServices = $("#manicurist").val();
-//         var street = $("#street").val();
-//         var city = $("#city").val();
-//         var county = $("#county").val();
-
-//         var bookedOrder = new Booking(fullname, service, timeForService, hairDressor, masseuse, otherServices, street, city, county);
-
-//         if (bookedOrder.service == "1" && bookedOrder.time == "1") {
-//             $("#label-one").show();
-//             yourOder = $("#hair-dressor").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-
-
-//         } else if (bookedOrder.service == "1" && bookedOrder.time == "2") {
-//             $("#label-one").show();
-//             yourOder = $("#hair-dressor").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-//         } else {
-//             $("#label-one").show();
-//             yourOder = $("#hair-dressor").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-//         }
-
-//         if (bookedOrder.service == "2" && bookedOrder.time == "2") {
-//             $("#label-two").show();
-//             yourOder = $("#masseuse").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-
-//         } else if (bookedOrder.service == "2" && bookedOrder.time == "2") {
-//             $("#label-two").show();
-//             yourOder = $("#masseuse").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-//         } else {
-//             $("#label-two").show();
-//             yourOder = $("#masseuse").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-//         }
-
-
-//         if (bookedOrder.service == "3" && bookedOrder.time == "3") {
-//             $("#label-three").show();
-//             yourOder = $("#manicurist").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-//         } else if (bookedOrder.service == "3" && bookedOrder.time == "3") {
-//             $("#label-three").show();
-//             yourOder = $("#manicurist").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-//         } else {
-//             $("#label-three").show();
-//             yourOder = $("#manicurist").show();
-//             $("#blank").html(hairDressor);
-//             $("#home").html(street);
-
-//         }
-
-
-
-//     });
-
-// });
