382994e04793f7a0c90439861a577652b0883327
Brian Kiplangat <brian.kiplangat@student.moringaschool.com>
2022-01-20 11:20:34 +0300

    edited contact.html

--- contact.js
+++ contact.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){
-    function verify(){
-        let name = document.getElementById('idname').value;
-        let email = document.getElementById('idemail').value;
-        if(name==''){
-            alert('input valid name');
-            return false
-        }
-        if(email==''){
-            alert('input valid email');
-            return false
-        }
-    alert('welcome '+name)
-    alert('your message has been received.Thanks')
-    });
+$(document).ready(function (){
+
+    //     let name = document.getElementById('#idname').value;
+    //     let email = document.getElementById('#idemail').value;
+    //     if(name==''){
+    //         alert('input valid name');
+    //         return false
+    //     }
+    //      else if(email==''){
+    //         alert('input valid email');
+    //         return false
+    //     }
+    //  else {alert('welcome '+name)
+    //        alert('your message has been received.Thanks')}
+    function one(){
+        alert('your message has been received.Thanks')}
+    })
+    
